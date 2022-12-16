fc2fe00b7c205666cdabe07246fb4d345a8e3707
Скогорев Эдуард <Skogor123@yandex.ru>
2022-12-16 20:07:06 +0300

    Upgrade main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from random import randint
 
-
-from graphic_arts.start_game_banner import run_screensaver
+"""Импорт библиотеки."""
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Вызов функции."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f'{5 + randint(3, 5)}')
@@ -18,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Вызов функции."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал'
                 f'{10 + randint(5, 10)} урона')
@@ -31,6 +32,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Вызов функции."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость'
                 f'{80 + 25}»')
@@ -44,6 +46,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Вызов функции."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -68,6 +71,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Вызов функции."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -89,8 +93,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-if __name__ == '__main__':
-    run_screensaver()
+def main():
+    """Вызов функции."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -100,3 +104,6 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+
+
+main()
